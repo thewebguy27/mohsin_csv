269b58fbdcbbe08834ab17fc033dc165483c134b
Muhammad Hamza <62599763+thewebguy27@users.noreply.github.com>
2020-12-22 03:38:07 -0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,5 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('Hello, world!')
+Hello, world!
+>>> 
